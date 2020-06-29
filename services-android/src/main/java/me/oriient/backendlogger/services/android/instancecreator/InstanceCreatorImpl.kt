037ce41d2fc9572c9086765190a1f6652c18b29a
baruch@oriient.me <baruch@oriient.me>
2020-06-29 15:04:30 +0300

    promoted sqldelight version

--- services-android/src/main/java/me/oriient/backendlogger/services/android/instancecreator/InstanceCreatorImpl.kt
+++ services-android/src/main/java/me/oriient/backendlogger/services/android/instancecreator/InstanceCreatorImpl.kt
@@ -1,12 +1,22 @@
 package me.oriient.backendlogger.services.android.instancecreator
 
+import android.util.Log
 import me.oriient.backendlogger.services.android.context
 import me.oriient.backendlogger.services.os.instancecreator.InstanceCreator
+import java.lang.reflect.InvocationTargetException
+
+private const val TAG = "InstanceCreator"
 
 @Suppress("unused")
 internal class InstanceCreatorImpl:
     InstanceCreator {
     override fun createInstance(name: String, vararg constructorArgs: Any): Any {
-        return Class.forName(name, false, context.classLoader).constructors.first().newInstance(*constructorArgs)
+        try {
+            return Class.forName(name, false, context.classLoader).constructors.first()
+                .newInstance(*constructorArgs)
+        } catch (e: InvocationTargetException) {
+            Log.e(TAG, "createInstance: cause: ${e.cause}, target: ${e.targetException}, message: ${e.message}", e)
+            throw e
+        }
     }
 }
